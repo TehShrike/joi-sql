f57bd740dd1a0c9c782485159bcc6b7c80328e43
Josh Duff <me@JoshDuff.com>
2015-04-23 22:15:48 -0500

    Works from the command-line

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-var build = require('./build')
-var fetch = require('./fetch')
-var mysql = require('mysql')
+var argv = require('minimist')(process.argv.slice(2))
 
-var db = mysql.createConnection({
-	host: '127.0.0.1',
-	user: 'root',
-	password: ''
-})
+if (typeof argv.schema !== 'string' || typeof argv.table !== 'string') {
+	console.error('you must pass in schema and table arguments')
+} else {
+	var build = require('./build')
+	var fetch = require('./fetch')
+	var mysql = require('mysql')
 
-fetch(db, { schema: 'pwner', table: 'project' }, function(err, columns) {
-	if (err) {
-		throw err
-	} else {
-		console.log(build(columns, true))
-	}
-})
+	var db = mysql.createConnection({
+		host: argv.host || '127.0.0.1',
+		user: argv.user || 'root',
+		password: argv.password || ''
+	})
+
+	fetch(db, { schema: argv.schema, table: argv.table }, function(err, columns) {
+		if (err) {
+			throw err
+		} else {
+			console.log(build(columns, argv.camel))
+		}
+		db.end()
+	})
+}
